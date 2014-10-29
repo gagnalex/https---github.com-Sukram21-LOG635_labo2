7bcc7a7c43945cec17559475f34e8809fbb60765
gagnalex <gagnalex@gmail.com>
2014-10-29 13:55:41 -0400

    Added basic .cfg and .py files

---
+++ emplois.py
@@ -0,0 +1,19 @@
+﻿# -*- coding: utf-8 -*-
+
+import nltk
+from nltk import *
+
+with open ("emplois.cfg", "r") as myfile:
+    grammaireText=myfile.read()
+
+grammar = grammar.FeatureGrammar.fromstring(grammaireText)
+parser = nltk.ChartParser(grammar)
+tokens = "Alice travaille pour Trio".split()
+
+
+parser = parse.FeatureEarleyChartParser(grammar)
+trees = parser.parse(tokens)
+for tree in trees:
+    print(tree)
+    nltk.draw.tree.draw_trees(tree)
+    print(tree.label()['SEM'])
